640f03a3857a8f847308867f2e6e571e591073c0
mtvalencia <mtv411@yahoo.com>
2019-06-02 23:33:26 -0600

    Adding first files

---
+++ seeds.sql
@@ -0,0 +1,6 @@
+/* Seeds for SQL table */
+USE bamazon;
+
+/* Insert 10 Rows into your new table */
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Blue Shirt", "Clothing", 12.00, 32);
