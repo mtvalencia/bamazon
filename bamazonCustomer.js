616aae85e7d180b72b5699c9afc3b351e1a0b825
mtvalencia <mtv411@yahoo.com>
2019-06-07 01:02:05 -0600

    Inquirer is working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 var moment = require('moment');
+var inquirer = require('inquirer');
 
 var mysql = require('mysql');
 
@@ -18,5 +19,36 @@ var con = mysql.createConnection({
       console.table(result);
       console.log("Inventory as of: " + moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
       console.log("Welcome to Bamazon!");
+      iq();
     });
   });
+
+  function iq() {
+    var questions = [
+      {
+        type: "input",
+        name: "product",
+        message:"Enter the ID of the product you would like to buy.",
+        validate: function(value) {
+        var valid = !isNaN(parseFloat(value));
+        return valid || 'Please enter a number';
+      },
+          filter: Number
+          },
+      {
+        type: "input",
+        name: "units",
+        message: "How many units of the product they would like to buy?",
+        validate: function(value) {
+        var valid = !isNaN(parseFloat(value));
+        return valid || 'Please enter a number';
+      },
+        filter: Number
+        }
+      ];
+  
+inquirer.prompt(questions).then(answers => {
+  console.log('\nYour order: ' + answers);
+  console.log(JSON.stringify(answers, null, '  '));
+});
+}
