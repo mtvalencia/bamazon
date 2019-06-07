0a7cc57a95e58100ff284f2ca4df63e76871e52a
mtvalencia <mtv411@yahoo.com>
2019-06-07 00:09:14 -0600

    Added moment and data

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,15 +1,22 @@
 require("dotenv").config();
+var moment = require('moment');
 
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
     host: "localhost",
+    port: 3306,
     user: "root",
-    password: process.env.mysql_password,
+    password: process.env.db_password,
     database: "bamazon"
   });
   
   con.connect(function(err) {
     if (err) throw err;
-    console.log("Connected!");
+    con.query("SELECT * FROM bamazon.products",  function (err, result, fields) {
+      if (err) throw err;
+      console.table(result);
+      console.log("Inventory as of: " + moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+      console.log("Welcome to Bamazon!");
+    });
   });
--- seeds.sql
+++ seeds.sql
@@ -4,3 +4,30 @@ USE bamazon;
 /* Insert 10 Rows into your new table */
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Blue Shirt", "Clothing", 12.00, 32);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Popcorn", "Food", 5.23, 11);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Hot Dogs", "Food", 2.00, 55);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Green Jacket", "Clothing", 54.99, 32);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("iPad", "Electronics", 799.00, 18);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Grapes", "Food", 3.00, 42);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Wireless Speaker", "Electronics", 120.00, 71);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Brown Hat", "Clothing", 45.00, 12);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Xbox", "Electronics", 99.00, 29);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Pizza", "Food", 16.00, 88);
