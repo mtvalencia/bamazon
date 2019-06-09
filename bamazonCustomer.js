c33e0f135cfceab2e905ab73c96ace4ddfa7e666
mtvalencia <mtv411@yahoo.com>
2019-06-09 12:54:07 -0600

    Changed question code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 var moment = require('moment');
 var inquirer = require('inquirer');
-
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
@@ -19,12 +18,12 @@ var con = mysql.createConnection({
       console.table(result);
       console.log("Inventory as of: " + moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
       console.log("Welcome to Bamazon!");
-      iq();
+      inq();
     });
   });
 
-  function iq() {
-    var questions = [
+  var inq = function() {
+    inquirer.prompt([
       {
         type: "input",
         name: "product",
@@ -38,17 +37,24 @@ var con = mysql.createConnection({
       {
         type: "input",
         name: "units",
-        message: "How many units of the product they would like to buy?",
+        message: "How many units?",
         validate: function(value) {
         var valid = !isNaN(parseFloat(value));
         return valid || 'Please enter a number';
       },
         filter: Number
-        }
-      ];
-  
-inquirer.prompt(questions).then(answers => {
-  console.log('\nYour order: ' + answers);
-  console.log(JSON.stringify(answers, null, '  '));
-});
+        }])
+        .then(function(answer) {
+          // based on their answer, either call the bid or the post functions
+          console.log(answer);
+        });
 }
+
+// function checkInv() {
+//   if (condition) {
+    
+//   }
+// }
+
+// console.log('\nYour order: ');
+// console.log(JSON.stringify(answers, null, '  '));
